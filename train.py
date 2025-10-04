5c55401ae0966b03215c17526ca18044549e9d2d
Ali Jafari <jafariali694@gmail.com>
2025-10-05 02:19:17 +0330

    Create train.py

---
+++ train.py
@@ -0,0 +1,108 @@
+import os
+import random
+import time
+import datetime
+import numpy as np
+import albumentations as A
+import cv2
+from PIL import Image
+from glob import glob
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from sklearn.model_selection import train_test_split
+import os
+
+def load_data(path):
+    train_x = sorted(glob(os.path.join(path,'train/images/*.png')))
+    train_y = sorted(glob(os.path.join(path,'train/masks/*.png')))
+
+    valid_x = sorted(glob(os.path.join(path,'valid/images/*.png')))
+    valid_y = sorted(glob(os.path.join(path,'valid/masks/*.png')))
+
+    test_x = sorted(glob(os.path.join(path,'test/images/*.png')))
+    test_y = sorted(glob(os.path.join(path,'test/masks/*.png')))
+
+    return [(train_x, train_y), (valid_x, valid_y), (test_x, test_y)]
+
+class DATASET(Dataset):
+    def __init__(self, images_path, masks_path, size, transform=None):
+        super().__init__()
+
+        self.images_path = images_path
+        self.masks_path = masks_path
+        self.size = size
+        self.transform = transform
+        self.n_samples = len(images_path)
+
+    def __getitem__(self, index):
+        """ Image """
+        image = cv2.imread(self.images_path[index], cv2.IMREAD_COLOR)
+        mask = cv2.imread(self.masks_path[index], cv2.IMREAD_GRAYSCALE)
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        image = cv2.resize(image, self.size)
+        image = np.transpose(image, (2, 0, 1))
+        image = image/255.0
+
+        mask = cv2.resize(mask, self.size)
+        mask = np.expand_dims(mask, axis=0)
+        mask = mask/255.0
+
+        return image, mask
+
+    def __len__(self):
+        return self.n_samples
+
+
+def train(model, loader, optimizer, loss_fn, device):
+    model.train()
+
+    epoch_loss = 0.0
+    epoch_jac = 0.0
+    epoch_f1 = 0.0
+    epoch_recall = 0.0
+    epoch_precision = 0.0
+
+    for i, (x, y) in enumerate(loader):
+        x = x.to(device, dtype=torch.float32)
+        y = y.to(device, dtype=torch.float32)
+
+        optimizer.zero_grad()
+        y_pred = model(x)
+        loss = loss_fn(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+        """ Calculate the metrics """
+        batch_jac = []
+        batch_f1 = []
+        batch_recall = []
+        batch_precision = []
+
+        y_pred = torch.sigmoid(y_pred)
+        for yt, yp in zip(y, y_pred):
+            score = calculate_metrics(yt, yp)
+            batch_jac.append(score[0])
+            batch_f1.append(score[1])
+            batch_recall.append(score[2])
+            batch_precision.append(score[3])
+
+        epoch_jac += np.mean(batch_jac)
+        epoch_f1 += np.mean(batch_f1)
+        epoch_recall += np.mean(batch_recall)
+        epoch_precision += np.mean(batch_precision)
+
+    epoch_loss = epoch_loss/len(loader)
+    epoch_jac = epoch_jac/len(loader)
+    epoch_f1 = epoch_f1/len(loader)
+    epoch_recall = epoch_recall/len(loader)
+    epoch_precision = epoch_precision/len(loader)
+
+    return epoch_loss, [epoch_jac, epoch_f1, epoch_recall, epoch_precision]
