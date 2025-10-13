41bf7f6a1ce7ec4ffc2b9f0b6700a562f6f028a1
Ali Jafari <jafariali694@gmail.com>
2025-10-13 16:01:02 +0330

    Update model.py

--- model.py
+++ model.py
@@ -24,29 +24,29 @@ class Squeeze_Excitation(nn.Module):
 class channel_attention_module(nn.Module):
     def __init__(self, ch, ratio=8):
         super().__init__()
-    
+
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
-    
+
         self.mlp = nn.Sequential(
             nn.Linear(ch, ch//ratio, bias=False),
             nn.ReLU(inplace=True),
             nn.Linear(ch//ratio, ch, bias=False)
         )
-    
+
         self.sigmoid = nn.Sigmoid()
-    
+
     def forward(self, x):
         x1 = self.avg_pool(x).squeeze(-1).squeeze(-1)
         x1 = self.mlp(x1)
-    
+
         x2 = self.max_pool(x).squeeze(-1).squeeze(-1)
         x2 = self.mlp(x2)
-    
+
         feats = x1 + x2
         feats = self.sigmoid(feats).unsqueeze(-1).unsqueeze(-1)
         refined_feats = x * feats
-    
+
         return refined_feats
 
 
@@ -118,37 +118,6 @@ class NonLocalBlock(nn.Module):
         z = W_y + x
         return z
 
-class MMBA(nn.Module):
-    def __init__(self, in_c):
-        '''in_channels of decoder part'''
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_c, 1, kernel_size=1)
-        self.sigmoid = nn.Sigmoid()
-        self.upsample = nn.Upsample(scale_factor=2)
-        self.conv2 = nn.Conv2d(in_c//2, in_c//4, kernel_size=1)
-        self.conv3 = nn.Conv2d(3*(in_c//4), in_c//2, kernel_size=3, padding=1)
-        self.se = Squeeze_Excitation(in_c//2)
-
-    def forward(self, e, d):
-        # e.g. encoder = torch.randn(2, 16, 32, 32) , decoder = torch.randn(2, 16, 16, 16)
-        d = self.conv1(d)
-        d = self.sigmoid(d)
-        d = self.upsample(d)
-
-        foreground = d * e
-        background = e * (1-d)
-        boundary = e * (1 - (2 * torch.abs(d-0.5)))
-
-        foreground = self.conv2(foreground)
-        background = self.conv2(background)
-        boundary = self.conv2(boundary)
-
-        out = torch.cat((foreground, background, boundary), dim=1)
-        out = self.conv3(out)
-        out = self.se(out)
-        out = e + out
-
-        return out
 
 class GCM(nn.Module):
     def __init__(self, in_c, out_c, rate=[3, 6, 9]):
@@ -196,32 +165,47 @@ class GCM(nn.Module):
         x = self.conv1(x)
         return x
 
+
+
+
+
 class Afa(nn.Module):
     def __init__(self, up_rate, in_c ):
         # in_c: number of encoder's channels
         super().__init__()
         self.in_c = in_c
         self.upsample1 = nn.Upsample(scale_factor=up_rate)
-        self.upsample2 = nn.Upsample(scale_factor=2)
-        self.non_local = NonLocalBlock(in_c*2)
+        self.non_local = NonLocalBlock(in_c)
 
-        self.conv = nn.Conv2d(in_c * 3 + 256, in_c*2, kernel_size=1)
+        self.conv_gcm = nn.Conv2d(1024, in_c, kernel_size=1)
 
-        self.se = Squeeze_Excitation(in_c*2)
+        self.conv_dec = nn.ConvTranspose2d(in_c*2 , in_c, kernel_size=2, stride=2, padding=0)
+
+
+        self.se = Squeeze_Excitation(in_c)
+
+        self.conv_features = nn.Sequential(
+            nn.Conv2d(in_c*3, in_c, kernel_size=3, padding=1),
+            nn.BatchNorm2d(in_c),
+            nn.ReLU(inplace=True)
+
+        )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_c*2, in_c*2, kernel_size=1),
-            nn.BatchNorm2d(in_c*2),
+            nn.Conv2d(in_c, in_c, kernel_size=1),
+            nn.BatchNorm2d(in_c),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, encoder, gcm, decoder):
         gcm = self.upsample1(gcm)
-        decoder = self.upsample2(decoder)
+        gcm = self.conv_gcm(gcm)
+        decoder = self.conv_dec(decoder)
+
         decoder2 = self.non_local(decoder)
 
         features = torch.cat((encoder, decoder2, gcm), dim=1)
-        features = self.conv(features)
+        features = self.conv_features(features)
         features = self.se(features)
 
         decoder = self.conv2(decoder)
@@ -229,12 +213,12 @@ class Afa(nn.Module):
         out = features + decoder
         return out
 
+
 class conv_block(nn.Module):
-    def __init__(self, in_c, out_c, stride=2, is_upsample=False):
+    def __init__(self, in_c, out_c):
         super().__init__()
-        self.is_upsample = is_upsample
-        self.upsample = nn.Upsample(scale_factor=2)
-        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1, stride = stride)
+
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(out_c)
 
         self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
@@ -243,8 +227,7 @@ class conv_block(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, inputs):
-        if self.is_upsample:
-            inputs = self.upsample(inputs)
+
         x = self.conv1(inputs)
         x = self.bn1(x)
         x = self.relu(x)
@@ -262,25 +245,38 @@ class encoder_block(nn.Module):
 
         self.conv = conv_block(in_c, out_c)
         self.cbam = cbam(out_c)
+        self.pool = nn.MaxPool2d(2)
 
     def forward(self, inputs):
         x = self.conv(inputs)
         x = self.cbam(x)
-        return x
+        p = self.pool(x)
+        return x, p
+
+
 
 class deep_supervision(nn.Module):
-    def __init__(self, in_c1, in_c2, in_c3, in_c4):
+    def __init__(self, in_c1, in_c2, in_c3, in_c4, in_c5):
         super().__init__()
         self.conv1 = nn.Conv2d(in_c1, 1, kernel_size=1)
         self.upsample1 = nn.Upsample(scale_factor=16)
+
         self.conv2 = nn.Conv2d(in_c2, 1, kernel_size=1)
         self.upsample2 = nn.Upsample(scale_factor=8)
+
         self.conv3 = nn.Conv2d(in_c3, 1, kernel_size=1)
         self.upsample3 = nn.Upsample(scale_factor=4)
+
         self.conv4 = nn.Conv2d(in_c4, 1, kernel_size=1)
-        self.conv5 = nn.Conv2d(4, 1, kernel_size=1)
-    
-    def forward(self, inp1, inp2, inp3, inp4):
+        self.upsample4 = nn.Upsample(scale_factor=2)
+
+        self.conv5 = nn.Conv2d(in_c5, 1, kernel_size=1)
+
+        self.conv_out = nn.Conv2d(5, 1, kernel_size=1)
+
+
+
+    def forward(self, inp1, inp2, inp3, inp4, inp5):
         out1 = self.conv1(inp1)
         out1 = self.upsample1(out1)
 
@@ -291,87 +287,100 @@ class deep_supervision(nn.Module):
         out3 = self.upsample3(out3)
 
         out4 = self.conv4(inp4)
+        out4 = self.upsample3(out4)
 
-        out = torch.cat([out1, out2, out3, out4], dim=1)
-        out = self.conv5(out)
+        out5 = self.conv5(inp5)
+        print(out1.shape)
+        print(out2.shape)
+        print(out3.shape)
+        print(out4.shape)
+        print(out5.shape)
+
+        out = torch.cat([out1, out2, out3, out4, out5], dim=1)
+        out = self.conv_out(out)
         return out
+
+
+
 
 class build_model(nn.Module):
     def __init__(self):
         super().__init__()
 
         """ Encoder """
-        self.e1 = encoder_block(3, 32)
-        self.e2 = encoder_block(32, 64)
-        self.e3 = encoder_block(64, 128)
-        self.e4 = encoder_block(128, 256)
-        self.e5 = encoder_block(256, 512)
+        self.e1 = encoder_block(3, 64)
+        self.e2 = encoder_block(64, 128)
+        self.e3 = encoder_block(128, 256)
+        self.e4 = encoder_block(256, 512)
+        self.e5 = encoder_block(512, 1024)
 
         """ Bottleneck """
-        self.gcm = GCM(512, 256)
+        self.gcm = GCM(1024, 1024)
 
         """ Decoder """
-        self.d5 = conv_block(512, 512, stride=1)
-        self.d4 = conv_block(512, 256, stride=1)
-        self.d3 = conv_block(256, 128, stride=1)
-        self.d2 = conv_block(128, 64, stride=1)
-        self.d1 = conv_block(64, 32, stride=1, is_upsample=True)
+        self.d5 = conv_block(1024, 1024)
+        self.d4 = conv_block(512, 512)
+        self.d3 = conv_block(256, 256)
+        self.d2 = conv_block(128, 128)
+        self.d1 = conv_block(64, 64)
 
         self.mmba4 = MMBA(512)
-        self.afa4 = Afa(2, 256)
+        self.afa4 = Afa(2, 512)
 
         self.mmba3 = MMBA(256)
-        self.afa3 = Afa(4, 128)
+        self.afa3 = Afa(4, 256)
 
         self.mmba2 = MMBA(128)
-        self.afa2 = Afa(8, 64)
+        self.afa2 = Afa(8, 128)
 
         self.mmba1 = MMBA(64)
-        self.afa1 = Afa(16, 32)
+        self.afa1 = Afa(16, 64)
 
-        self.deep_supervision = deep_supervision(256, 128, 64, 32)
+        self.deep_supervision = deep_supervision(1024, 512, 256, 128, 64)
 
         """ Classifier """
-        self.outputs = nn.Conv2d(32, 1, kernel_size=1, padding=0)
 
     def forward(self, inputs):
         """ Encoder """
-        e1 = self.e1(inputs)    # e1.shape torch.Size([2, 32, 128, 128])
-        e2 = self.e2(e1)        # e2.shape torch.Size([2, 64, 64, 64])
-        e3 = self.e3(e2)        # e3.shape torch.Size([2, 128, 32, 32])
-        e4 = self.e4(e3)        # e4.shape torch.Size([2, 256, 16, 16])
-        e5 = self.e5(e4)        # e5.shape torch.Size([2, 512, 8, 8])
+        s1, p1 = self.e1(inputs)    # s1.shape torch.Size([2, 64, 256, 256]), s1.shape torch.Size([2, 64, 128, 128])
+        s2, p2 = self.e2(p1)        # s2.shape torch.Size([2, 128, 128, 128])
+        s3, p3 = self.e3(p2)        # s3.shape torch.Size([2, 256, 64, 64])
+        s4, p4 = self.e4(p3)        # s4.shape torch.Size([2, 512, 32, 32])
+        s5, p5 = self.e5(p4)        # s5.shape torch.Size([2, 1024, 16, 16])
 
         """ Bottleneck """
-        gcm = self.gcm(e5)          # gcm torch.Size([2, 256, 8, 8])
+        gcm = self.gcm(s5)          # gcm.shape:  torch.Size([2, 1024, 16, 16])
+
         """ Decoder """
-        d5 = self.d5(e5)      # d5.shape torch.Size([2, 512, 8, 8])
+        d5 = self.d5(s5)            # d5.shape:  torch.Size([2, 1024, 16, 16])
 
-        mmba4 = self.mmba4(e4, d5)   # mmba4.shape torch.Size([2, 256, 16, 16])
-        afa4 = self.afa4(mmba4, gcm, d5)   # afa4.shape torch.Size([2, 512, 16, 16])
 
-        d4 = self.d4(afa4)   # d4.shape torch.Size([2, 256, 16, 16])
-        # print('d4.shape', d4.shape)
+        mmba4 = self.mmba4(s4, d5)   # mmba4.shape:  torch.Size([2, 512, 32, 32])
 
-        mmba3 = self.mmba3(e3, d4)  # mmba3.shape torch.Size([2, 128, 32, 32])
-        afa3 = self.afa3(mmba3, gcm, d4)   # afa3.shape torch.Size([2, 256, 32, 32])
+        afa4 = self.afa4(mmba4, gcm, d5)    # afa4.shape:  torch.Size([2, 512, 32, 32])
 
-        d3 = self.d3(afa3)   # d3.shape torch.Size([2, 128, 32, 32])
-        # print('d3.shape', d3.shape)
+        d4 = self.d4(afa4)         # d4.shape torch.Size([2, 512, 32, 32])
 
-        mmba2 = self.mmba2(e2, d3)   # mmba2.shape torch.Size([2, 64, 64, 64])
-        afa2 = self.afa2(mmba2, gcm, d3)   # afa2.shape torch.Size([2, 128, 64, 64])
+        mmba3 = self.mmba3(s3, d4)        # mmba3.shape:  torch.Size([2, 256, 64, 64])
 
-        d2 = self.d2(afa2)  # d2.shape torch.Size([2, 64, 64, 64])
-        # print('d2.shape', d2.shape)
+        afa3 = self.afa3(mmba3, gcm, d4)    # afa3.shape:  torch.Size([2, 256, 64, 64])
 
-        mmba1 = self.mmba1(e1, d2)   # mmba1.shape torch.Size([2, 32, 128, 128])
-        afa1 = self.afa1(mmba1, gcm, d2)   # afa1.shape torch.Size([2, 64, 128, 128])
-        d1 = self.d1(afa1)   # d1.shape torch.Size([2, 32, 128, 128])
-        # print('d1.shape', d1.shape)
+        d3 = self.d3(afa3)         # d3.shape:  torch.Size([2, 256, 64, 64])
 
-        """ Classifier """
-        # outputs = self.outputs(d1)
-        outputs = self.deep_supervision(d4, d3, d2, d1)
 
-        return outputs
+        mmba2 = self.mmba2(s2, d3)         # mmba.shape:  torch.Size([2, 128, 128, 128])
+        afa2 = self.afa2(mmba2, gcm, d3)   # afa2.shape:  torch.Size([2, 128, 128, 128])
+
+        d2 = self.d2(afa2)   # d2.shape:  torch.Size([2, 128, 128, 128])
+
+        mmba1 = self.mmba1(s1, d2)
+        afa1 = self.afa1(mmba1, gcm, d2)
+        d1 = self.d1(afa1)   # d1.shape:  torch.Size([2, 64, 256, 256])
+
+
+
+        # """ Classifier """
+        outputs = self.deep_supervision(d5, d4, d3, d2, d1)
+
+        # return outputs
+
